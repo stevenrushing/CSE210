dd394585cd2f235195ca2bca2c2925202d8f5994
Steven Rushing <steven@stevenrushing.com>
2025-11-03 13:58:50 +0100

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What was your grade? ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+        string sign = "";
+
+        if (grade < 95 && grade > 60) {
+            if ((grade % 10) >= 7) {
+                sign = "+";
+            }
+
+            if ((grade % 10) < 3)
+            {
+                sign = "-";
+            }
+        }
+
+
+
+        if (grade >= 90) {
+            Console.WriteLine($"Your grade is an A{sign}");
+        }
+        else if (grade > 80) {
+            Console.WriteLine($"Your grade is a B{sign}");
+        }
+        else if (grade > 70) {
+            Console.WriteLine($"Your grade is a C{sign}");
+        }
+        else if (grade > 60) {
+            Console.WriteLine($"Your grade is a D{sign}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is an F");
+        }
+
     }
 }
