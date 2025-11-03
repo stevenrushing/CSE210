89a1901e1fcf34941e52b06206f7b67703e373d1
Steven Rushing <steven@stevenrushing.com>
2025-11-03 15:08:01 +0100

    exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Random randomGenerator = new Random();
+            int secret = randomGenerator.Next(1, 11);
+
+            int guess = 0;
+
+            while (guess != secret)
+            {
+                Console.Write("What is your guess? ");
+                string guessString = Console.ReadLine();
+                guess = int.Parse(guessString);
+
+                if (guess < secret)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > secret)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            Console.Write("Would you like to play again? (yes/no) ");
+            response = Console.ReadLine();
+        }
+
     }
 }
+
+
+// What is the magic number? 18
+//   What is your guess? 5
+//   Higher
+//   What is your guess? 6
+//   Higher
+//   What is your guess? 7
+//   Higher
+//   What is your guess? 20
+//   Lower
+//   What is your guess? 19
+//   Lower
+//   What is your guess? 18
+//   You guessed it!
