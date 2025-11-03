b73312284d36cdb5ca3e2c88c986a4483a127ecd
Steven Rushing <steven@stevenrushing.com>
2025-11-03 13:02:41 +0100

    First exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
+
+// What is your first name? Scott
+// What is your last name? Burton
+
+// Your name is Burton, Scott Burton.
