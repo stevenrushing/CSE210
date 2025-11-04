e26eaeea695c54415b505009db3ff2532edfb3db
Steven Rushing <steven@stevenrushing.com>
2025-11-04 09:36:02 +0100

    created learning folder

---
+++ week01/Learning/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+    }
+}
+
+// What is your first name? Scott
+// What is your last name? Burton
+
+// Your name is Burton, Scott Burton.
