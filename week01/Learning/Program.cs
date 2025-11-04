45e55e6e712be0ff29397c9117db02fd96a9b41c
Steven Rushing <steven@stevenrushing.com>
2025-11-04 10:19:32 +0100

    learning

--- week01/Learning/Program.cs
+++ week01/Learning/Program.cs
@@ -1,20 +1,125 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
+        // Console.Write("What is your first name? ");
+        // string firstName = Console.ReadLine();
+        // Console.Write("What is your last name? ");
+        // string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
-    
-        int x;
-        x = 5;
-        Console.WriteLine(x);
-    
+        // Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+        // int x;
+        // x = 5;
+        // Console.WriteLine(x);
+
+        // bool booleanValue;
+        // booleanValue = true;
+        // Console.WriteLine(booleanValue);
+
+        // x = 10;
+
+        // Console.WriteLine(x);
+        // int y = 5;
+
+        // if (x > y)
+        // {
+        //     Console.WriteLine("greater");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("lesser");
+        // }
+
+
+        // if (name == "Peter" || name == "James" || name == "John")
+        // {
+        //     Console.WriteLine("This is a biblical name.");
+        // }
+
+
+        // if (firstName == "Brigham" && lastName == "Young")
+        // {
+        //     Console.WriteLine("Welcome Brother Brigham!");
+        // }
+
+
+        // if (!(name == "Peter" || name == "James" || name == "John"))
+        // {
+        //     Console.WriteLine("This is a not one of those three");
+        // }
+
+        // Console.Write("What is your favorite number? ");
+        // string userInput = Console.ReadLine();
+        // int number = int.Parse(userInput);
+
+
+        // int number = 42;
+        // string textVersion = number.ToString();
+
+        // string response = "yes";
+
+        // while (response == "yes")
+        // {
+        //     Console.Write("Do you want to continue? ");
+        //     response = Console.ReadLine();
+        // }
+
+        // string response;
+
+        // do
+        // {
+        //     Console.Write("Do you want to continue? ");
+        //     response = Console.ReadLine();
+        // } while (response == "yes");
+
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        // Above and below initialize variable, then condition, then increment
+
+        // for (int i = 2; i <= 20; i = i + 2)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        // foreach (string color in colors)
+        // {
+        //     Console.WriteLine(color);
+        // }
+
+        // Random randomGenerator = new Random();
+        // int number = randomGenerator.Next(1, 11);
+
+        // List<int> numbers = new List<int>();
+        // List<string> words = new List<string>();
+
+        // List<string> words = new List<string>();
+
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("mouse");
+
+
+        // Console.WriteLine(words.Count);
+        // print(len(words))
+
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+
+        // below, access by index:
+        // for (int i = 0; i < words.Count; i++)
+        // {
+        //     Console.WriteLine(words[i]);
+        // }
+
     }
 }
 
